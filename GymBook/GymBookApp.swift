5c0765719396985f29e322c32f6036a9e1213d2a
Rustam Cherezbiev <rcherezbiev@gmai.com>
2024-03-17 23:06:25 +0300

    Initial Commit

---
+++ GymBook/GymBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBookApp.swift
+//  GymBook
+//
+//  Created by Rustam Cherezbiev on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GymBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
